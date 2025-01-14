f8ea28ffb6cf683ef5c376ecb518945f727a2f42
Ahmet Efe Kurtkapan <ahmetkurtkapan@outlook.com>
2025-01-14 16:16:49 +0300

    Add files via upload

---
+++ Nesneye Dayalı Programlama-II Final Projesi/src/JsonUtil.java
@@ -0,0 +1,30 @@
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class JsonUtil {
+
+    private static final Gson gson = new Gson();
+
+    public static <T> void writeToFile(String filePath, List<T> data) {
+        try (FileWriter writer = new FileWriter(filePath)) {
+            gson.toJson(data, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static <T> List<T> readFromFile(String filePath, Type type) {
+        try (FileReader reader = new FileReader(filePath)) {
+            return gson.fromJson(reader, type);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
